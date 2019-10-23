2f3985c2e15ebf995c598d6dda3f3f0568f627b5
shivam <sharma.shivam7770@gmail.com>
2019-10-23 12:11:25 +0530

    Sum the root to leaf in a binary tree

--- epi/SumRootToLeaf.java
+++ epi/SumRootToLeaf.java
@@ -2,11 +2,22 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class SumRootToLeaf {
-  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
+
+  public static int solution(BinaryTreeNode<Integer> tree,int partialSum){
+      if(tree==null){
+        return 0;
+      }
+      partialSum=partialSum*2+tree.data;
+      if(tree.left==null && tree.right==null){
+        return partialSum;
+      }
+      return solution(tree.left,partialSum)+solution(tree.right,partialSum);
+  }
+  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
   public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
+
+    return solution(tree,0);
   }
 
   public static void main(String[] args) {
