f81025035727c04ac82d3d6cafb521c29a919746
shivam <sharma.shivam7770@gmail.com>
2019-10-23 15:43:24 +0530

    Tree inorder with iteration

--- epi/TreeInorder.java
+++ epi/TreeInorder.java
@@ -5,6 +5,8 @@ import epi.test_framework.GenericTest;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
+
 public class TreeInorder {
 
   @EpiTest(testDataFile = "tree_inorder.tsv")
@@ -12,8 +14,24 @@ public class TreeInorder {
 
   public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {  //left root rigt
       List<Integer> ls=new ArrayList();
-      Inorder(tree,ls);
+//      Inorder(tree,ls);
+      InorderWithIteration(tree,ls);
     return ls;
+  }
+  static void InorderWithIteration(BinaryTreeNode<Integer> root,List<Integer> ls){
+      Stack<BinaryTreeNode> stack = new Stack();
+
+
+      while(!stack.isEmpty() || root!=null){
+
+          while(root!=null){
+              stack.push(root);
+              root=root.left;
+          }
+          BinaryTreeNode node=stack.pop();
+          ls.add((Integer) node.data);
+          root=node.right;
+      }
   }
    static  void Inorder(BinaryTreeNode<Integer> tree,List<Integer> ls){
       if(tree!=null){
