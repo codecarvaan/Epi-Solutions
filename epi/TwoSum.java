5bfa8c4c2459acb7e7c346a73e16fa90ad564782
shivam <sharma.shivam7770@gmail.com>
2019-12-06 17:40:43 +0530

    two sum

--- epi/TwoSum.java
+++ epi/TwoSum.java
@@ -1,20 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 import java.util.List;
+
 public class TwoSum {
-  @EpiTest(testDataFile = "two_sum.tsv")
+    @EpiTest(testDataFile = "two_sum.tsv")
 
-  public static boolean hasTwoSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
-  }
+    public static boolean hasTwoSum(List<Integer> A, int t) {
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "TwoSum.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        int start = 0;
+        int end = A.size() - 1;
+
+        while (start <= end) {
+            int sum = A.get(start) + A.get(end);
+
+            if (sum == t) {
+                return true;
+            } else if (sum > t) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "TwoSum.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
